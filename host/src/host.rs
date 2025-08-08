3e186f9de288433f2a65069d9ce612bc1e738816
Tomer-Eliahu <73798700+Tomer-Eliahu@users.noreply.github.com>
2025-08-08 19:56:33 +0000

    attempt

--- host/src/host.rs
+++ host/src/host.rs
@@ -1068,13 +1068,13 @@ impl<'d, C: Controller, P: PacketPool> ControlRunner<'d, C, P> {
         host.connections
             .set_link_credits(ret.total_num_le_acl_data_packets as usize);
 
-        const ACL_LEN: u16 = 1021; //was originally 255, 
-        const ACL_N: u16 = 10;
+        const ACL_LEN: u16 = 50; //was originally 255, 
+        const ACL_N: u16 = 1;
         info!(
             "[host] configuring host buffers ({} packets of size {})",
             ACL_N, ACL_LEN,
         );
-        //HostBufferSize::new(ACL_LEN, 0, ACL_N, 0).exec(&host.controller).await?;
+        HostBufferSize::new(ACL_LEN, 0, ACL_N, 0).exec(&host.controller).await?;
 
         /*
                 #[cfg(feature = "controller-host-flow-control")]
